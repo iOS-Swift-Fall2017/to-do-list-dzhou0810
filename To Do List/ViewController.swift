aeb17c265514b290a43e30707d88cf6662238211
Dingnan Zhou <dingnanzhou@dingnans-mbp.bc.edu>
2017-09-26 14:57:15 -0400

    Hi Angela, I had a technical issue which keeps crashing my app. Just got fixed this afternoon at the office Hour, and the TA (not Sazan, probably Nick) recommended me to ask for an extension. I will submit my completed work ASAP. Probably tonight by midnight, Tomorrow the latest.
    Best,
    
    Dingnan Zhou

--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoArray = ["Learn Swift", "Build Apps", "Change the World!"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+          tableView.delegate = self
+          tableView.dataSource = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "EditItem" {
+            let destination = segue.destination as!
+            DetailViewController
+            let index =  tableView.indexPathForSelectedRow!.row
+            destination.toDoItem = toDoArray[index]
+        }
+    }
+    @IBAction func unwindFromDetailViewController(seque: UIStoryboardSegue){
+        
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoArray.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = toDoArray[indexPath.row]
+        return cell
+   }
 }
 
